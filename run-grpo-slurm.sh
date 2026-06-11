28a604f7d0f9beedeb5aa26301204115cbdeb9c5
Jeremi Piotrowski <jpiotrowski@nvidia.com>
2026-06-11 07:21:48 -0700

    fix slurm submit script

--- run-grpo-slurm.sh
+++ run-grpo-slurm.sh
@@ -4,13 +4,13 @@ NUM_ACTOR_NODES=1
 
 # grpo_math_8b uses Llama-3.1-8B-Instruct model
 COMMAND="./run-grpo.sh" \
-CONTAINER=./nvcr.io/nvidia/nemo-rl:v0.6.0
+CONTAINER=./nvcr.io/nvidia/nemo-rl:v0.6.0 \
 MOUNTS="$PWD:$PWD" \
 sbatch \
     --nodes=${NUM_ACTOR_NODES} \
-    --account=YOUR_ACCOUNT \
-    --job-name=YOUR_JOBNAME \
-    --partition=YOUR_PARTITION \
-    --time=4:0:0 \
+    --account=coreai_mlperf_training \
+    --job-name=coreai_mlperf_training-grpo.nemotron9b-workplace-assistant \
+    --partition=batch_short \
+    --time=2:0:0 \
     --gres=gpu:8 \
     ray.sub
